6ee46fecc1cd853bcdf0cd525c7083bf1c4c5592
Alexander Laba <alexander.laba@bd.qatarinsurance.com>
2026-03-19 17:00:14 +0800

    Add handlers - iteration 3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/alexander-xyz/metrics/internal/handler"
 	"github.com/alexander-xyz/metrics/internal/repository"
 )
 
-func run() error {
+func main() {
 	store := repository.NewMemStorage()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/`, handler.UpdateMetricHandler(store))
-
-	return http.ListenAndServe(`:8080`, mux)
-}
-
-func main() {
-	if err := run(); err != nil {
+	if err := handler.RunServer(store); err != nil {
 		panic(err)
 	}
 }
